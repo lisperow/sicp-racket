d6768b314981f1e54f122e778af5107a3443919b
lisperow <lisperow@yandex.ru>
2019-10-01 18:19:05 +0300

    add 0.8.rkt

---
+++ sicp/chapter1/08.rkt
@@ -0,0 +1,24 @@
+#lang sicp
+
+(#%require rackunit)
+
+(define (square x) (* x x))
+
+(define (qbiter guess x)
+  (if (good-enough? guess x)
+      guess
+      (qbiter (improve guess x)
+              x)))
+
+(define (good-enough? guess x)
+  (< (abs (- (qube guess) x)) 0.001))
+
+(define (cube x) (* x x x))
+
+(define (improve guess x)
+  (/ (+ (/ x (square guess))
+        (* 2 guess))
+      3))
+
+(define (qbrt x)
+  (qbiter 1.0 x))      
