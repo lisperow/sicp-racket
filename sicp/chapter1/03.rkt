4cd451e8f8dc14986d04232986027fca7308b2b9
lisperow <lisperow@yandex.ru>
2019-10-01 14:39:46 +0300

    fix

--- sicp/chapter1/03.rkt
+++ sicp/chapter1/03.rkt
@@ -9,8 +9,8 @@
   (+ (square x) (square y)))
 
 (define (solution x y z)
-  (cond ((and (< x y) (x < z)) (sum-of-square y z))
-        ((and (< y x) (y < z)) (sum-of-square x z))
-        (else (sum-of-square(x y))))
+  (cond ((and (< x y) (< x z)) (sum-of-square y z))
+        ((and (< y x) (< y z)) (sum-of-square x z))
+        (else (sum-of-square x y))))
 
 (check-equal? (solution 2 3 4) 25)
